a5db62972344fb0b4806d0ce6632eed36aecb98d
Watcharin Sarachai <watcharin_s@mju.ac.th>
2024-02-05 22:10:19 +0700

    update

--- src/main/java/org/itsci/it10306103/lesson09/ex01/Course.java
+++ src/main/java/org/itsci/it10306103/lesson09/ex01/Course.java
@@ -1,6 +1,5 @@
 package org.itsci.it10306103.lesson09.ex01;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
---
+++ src/main/java/org/itsci/it10306103/lesson09/ex01/TestApp2.java
@@ -0,0 +1,34 @@
+package org.itsci.it10306103.lesson09.ex01;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+public class TestApp2 {
+  public static void main(String[] args) {
+    SessionFactory sessionFactory = HibernateConnection.getSessionFactory();
+    Session session = sessionFactory.openSession();
+
+    try {
+      session.beginTransaction();
+      Student student = session.get(Student.class, 1);
+      for (Course course : student.getCourses()) {
+        System.out.println("Course ID: " + course.getId());
+        System.out.println("Course Name: " + course.getName());
+        System.out.println("Course Credit: " + course.getCredit());
+      }
+
+      student = session.get(Student.class, 2);
+      for (Course course : student.getCourses()) {
+        System.out.println("Course ID: " + course.getId());
+        System.out.println("Course Name: " + course.getName());
+        System.out.println("Course Credit: " + course.getCredit());
+      }
+      session.getTransaction().commit();
+    } finally {
+      if (session.getTransaction().isActive()) {
+        session.getTransaction().rollback();
+      }
+      session.close();
+    }
+  }
+}
